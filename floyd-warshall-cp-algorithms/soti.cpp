75b7836dc3666f392b30f90e43e776b1556f7a46
Vishnu V V <vishnuvvnivis@gmail.com>
2024-11-30 13:17:41 +0530

    ..

--- floyd-warshall-cp-algorithms/soti.cpp
+++ floyd-warshall-cp-algorithms/soti.cpp
@@ -4,98 +4,121 @@ using namespace std;
 #define INF 1e9
 #define ll long long
 
-// Floyd-Warshall Algorithm
-void floydWarshall(vector<vector<ll>>& dist, int n) {
-    for (int k = 1; k <= n; ++k) {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (dist[i][k] != INF && dist[k][j] != INF) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
+/**
+ * input:
+ * 
+ * 7 9
+1 2 1
+2 3 4
+3 4 4
+4 5 2
+5 1 1
+1 6 1
+6 7 2
+7 4 7
+5 3 1
+ * 
+ * 
+output:
+
+0 1 2 3 1 1 3 
+1 0 3 4 2 2 4 
+2 3 0 3 1 3 5 
+3 4 3 0 2 4 6 
+1 2 1 2 0 2 4 
+1 2 3 4 2 0 2 
+3 4 5 6 4 2 0 
+8 //length of weights used
+1 2
+1 5
+1 6
+3 5
+4 5
+6 7
+
+ * 
+ */
+
+
+vector<vector<int>>floydWarshall(vector<vector<int>>mat,int n){
+
+    for(int i=1;i<=n;i++){
+        mat[i][i]=0;
+    }
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(mat[i][k]!=1e9 || mat[k][j]!=1e9)
+               
+                    mat[i][j] =min(mat[i][j], mat[i][k] + mat[k][j]);
+                
             }
         }
     }
-
-
-    for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                cout<<dist[i][j]<<" ";
-            }
-
-            cout<<endl;
-        }
-
-
-
+return mat;
 }
 
-int main() {
-    freopen("./input.txt", "r", stdin);
-    freopen("./output.txt", "w", stdout);
 
-    int n, m; // Number of nodes and edges
-    cin >> n >> m;
+int main(){
+ freopen("./input.txt","r",stdin);
+    freopen("./output.txt","w",stdout);
 
-    vector<vector<ll>> dist(n + 1, vector<ll>(n + 1, INF));
-    vector<tuple<int, int, ll>> edges; // Store edges as (u, v, w)
+    int n,m;
 
-    // Initialize distances
-    for (int i = 1; i <= n; ++i) {
-        dist[i][i] = 0;
+    cin>>n>>m;
+
+    int u,v,wt;
+
+    int totWt=0;
+    vector<vector<int>>mat(n+1,vector<int>(n+1,1e9));
+    vector<pair<int,pair<int,int>>>edges;
+    while(m--){
+        cin>>u>>v>>wt;
+        totWt+=wt;
+        mat[u][v] =wt;
+        mat[v][u] = wt;
+        edges.push_back({u,{v,wt}});
     }
 
-    // Input edges
-    for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        edges.push_back({u, v, w});
-        dist[u][v] = min(dist[u][v], (ll)w);
-        dist[v][u] = min(dist[v][u], (ll)w);
+    mat = floydWarshall(mat,n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
     }
 
-    // Run Floyd-Warshall
-    floydWarshall(dist, n);
+    set<pair<int,int>>true_edge;
+int req_wt=0;
 
-    ll total_weight = 0;
-    set<pair<int, int>> important_edges;
-
-    // Check each edge
-    for (auto& edge : edges) {
-        int u, v;
-        ll w;
-        tie(u, v, w) = edge;
-
-        bool is_important = false;
-
-        // Check if edge (u, v) is part of any shortest path
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (dist[i][j] == dist[i][u] + w + dist[v][j] ||
-                    dist[i][j] == dist[i][v] + w + dist[u][j]) {
-                    is_important = true;
-                    break; // No need to check further for this pair
+    for(auto edge:edges){
+        bool found =false;
+        u = edge.first;
+        v = edge.second.first;
+        wt = edge.second.second;
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                  if (mat[i][j] == mat[i][u] + wt + mat[v][j] || mat[i][j] == mat[i][v] + wt + mat[u][j]) {
+                    
+                    true_edge.insert({u,v});
+                    req_wt+=wt;
+                    found=true;
+                    break;
+                 }
+                
                 }
+                 if(found)break;   
             }
-           // if (is_important) break; // No need to check further nodes
-        }
-
-        if (is_important) {
-            important_edges.insert({min(u, v), max(u, v)});
-            total_weight += w; // Use the input weight directly
-        }
+       
+            
+        
     }
 
-    // Output the total weight of important edges
-    cout << total_weight << endl;
+    cout<<req_wt<<endl;
 
-   
-    
-
-
-    for(auto edge:important_edges){
-        cout<<edge.first<<" "<<edge.second<<endl;
+    for(auto edg:true_edge){
+        cout<<edg.first<<" "<<edg.second<<endl;
     }
 
-
-    return 0;
 }
